d9d39e0b645373d4df71dca372459f24b2aba62c
Gopinath Rangappa <mr.gopinath@gmail.com>
2013-08-06 04:20:21 +0000

    initial layout with theme

--- web.js
+++ web.js
@@ -4,11 +4,15 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname));
+
+/*
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
   var buf = fs.readFileSync('index.html');
   response.send(buf.toString());
 });
+*/
 
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
